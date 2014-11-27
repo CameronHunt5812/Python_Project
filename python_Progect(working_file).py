a5d6b347dacd50c7420da7c178263aa7d00926b5
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-11-27 16:29:54 -0600

    added new files

---
+++ python_Progect(working_file).py
@@ -0,0 +1,38 @@
+for y in range (len(position)):
+    for x in range (len(position[y])):
+        for word in range (len(lookFor))
+            if position[y][x] == lookFor[0][0]:
+                for char in (len(lookFor)):
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+def check(y,x,p):
+    if p == 0:
+        if position[y-1][x-1] == char:
+            
+    elif p == 1:
+        if position[y-1][x] == char:
+    elif p == 2:
+        if position[y-1][x+1] == char:
+    elif p == 3:
+        if position[y][x-1] == char:
+    elif p == 4:
+        if position[y][x+1] == char:
+    elif p == 5:
+        if position[y+1][x-1] == char:
+    elif p == 6:
+        if position[y+1][x] == char:
+    elif p == 7:
+        if position[y+1][x+1] == char:
