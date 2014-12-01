1124000ef9c339c8dc4109829fcf5402a085a8a3
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-12-01 16:06:52 -0600

    added many if statments to look around the curent position
    need to resolve the eror on line 69 70 and 75

--- python_prgect.py
+++ python_prgect.py
@@ -1,4 +1,5 @@
 f = open("E:\\wordsearch.txt","r")
+direction = 0
 #Find the height, width and how many words to look for in the file.
 height = int(f.readline())
 width = int(f.readline()) 
@@ -17,6 +18,105 @@ print position
 #make a list of the words that you are looking for
 lookFor = [f.readline() for w in range(numOfWords)]
 print lookFor
-
-
-                    
+for y in range (len(position)):
+    for x in range (len(position[y])):
+        for word in range (len(lookFor)):
+            if position[y][x] == lookFor[word][0]:
+                if y == 0 and x == 0:
+                    print "top left" + str(lookFor[word][0])
+                    if position[y][x+1] == lookFor[word][1]:
+                        direction = 5
+                    elif position[y+1][x] == lookFor[word][1]:
+                        direction = 7
+                    elif position[y+1][x+1] == lookFor[word][1]:
+                        direction = 8
+                    else:
+                        print "not here"
+                elif y == 0 and x == len(position[y]):
+                    print "top right" + str(lookFor[word][0])
+                    if position[y][x-1] == lookFor[word][1]:
+                        direction = 4
+                    elif position[y+1][x-1] == lookFor[word][1]:
+                        direction = 6
+                    elif position[y+1][x] == lookFor[word][1]:
+                        direction = 7
+                    else:
+                        print "not here"
+                elif y == len(position) and x == 0:
+                    print "botom leaft" + str(lookFor[word][0])
+                    if position[y-1][x] == lookFor[word][1]:
+                        direction = 2
+                    elif position[y-1][x+1] == lookFor[word][1]:
+                        direction = 3
+                    elif position[y][x+1] == lookFor[word][1]:
+                        direction = 5
+                    else:
+                        print "not here"
+                elif y == len(position) and x == len(position[y]):
+                    print "botom right" + str(lookFor[word][0])
+                    if position[y-1][x] == lookFor[word][1]:
+                        direction = 1
+                    elif position[y-1][x-1] == lookFor[word][1]:
+                        direction = 2
+                    elif position[y][x-1] == lookFor[word][1]:
+                        direction = 4
+                    else:
+                        print "not here"  
+                elif y == 0 and x+1 <= len(position[y]):
+                    print "top" + str(lookFor[word][0])
+                    if position[y][x-1] == lookFor[word][1]:
+                        direction = 4
+                    elif position[y][x-1] == lookFor[word][1]:
+                        direction = 5
+                    elif position[y+1][x-1] == lookFor[word][1]:
+                        direction = 6
+                    elif position[y+1][x] == lookFor[word][1]:
+                        direction = 7
+                    elif position[y+1][x-1] == lookFor[word][1]:
+                        direction = 8
+                    else:
+                        print position[y][x+1]
+                elif y == len(position):
+                    print "botom" + str(lookFor[word][0])
+                    if position[y-1][x-1] == lookFor[word][1]:
+                        direction = 1
+                    elif position[y-1][x] == lookFor[word][1]:
+                        direction = 2
+                    elif position[y-1][x+1] == lookFor[word][1]:
+                        direction = 3
+                    elif position[y][x-1] == lookFor[word][1]:
+                        direction = 4
+                    elif position[y][x+1] == lookFor[word][1]:
+                        direction = 5
+                    else:
+                        print "not here"
+                elif x == 0:
+                    print "left side" + str(lookFor[word][0])
+                    if position[y-1][x] == lookFor[word][1]:
+                        direction = 2
+                    elif position[y-1][x+1] == lookFor[word][1]:
+                        direction = 3
+                    elif position[y][x+1] == lookFor[word][1]:
+                        direction = 5
+                    elif position[y+1][x] == lookFor[word][1]:
+                        direction = 6
+                    elif position[y+1][x+1] == lookFor[word][1]:
+                        direction = 7
+                    else:
+                        print "not here"
+                elif x == len(position[y]):
+                    print "right side" + str(lookFor[word][0])
+                    if position[y-1][x] == lookFor[word][1]:
+                        direction = 1
+                    elif position[y-1][x-1] == lookFor[word][1]:
+                        direction = 2
+                    elif position[y][x-1] == lookFor[word][1]:
+                        direction = 4
+                    elif position[y+1][x-1] == lookFor[word][1]:
+                        direction = 6
+                    elif position[y+1][x] == lookFor[word][1]:
+                        direction = 7
+                    else:
+                        print "not here"
+                #print "it got here"
+                #for char in range (len(lookFor[word])):
