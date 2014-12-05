78371e376be8882776afa6ac948587763256ae28
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-12-05 00:12:08 -0600

    the program is now somewhat working
    need to find a way to
    when a word is not found, loop back to look for other words with the
    same first letter.

--- python_prject.py
+++ python_prject.py
@@ -87,41 +87,72 @@ def search(side,position,y,x,):
             direction = "DL"
         elif position[y+1][x] == SecondLetter:
             direction = "D"
+    elif side == "middle":
+        if position[y-1][x-1] == SecondLetter:
+            direction = "upL"
+        elif position[y-1][x] == SecondLetter:
+            direction = "up"
+        elif position[y-1][x+1] == SecondLetter:
+            direction = "upR"
+        elif position[y][x-1] == SecondLetter:
+            direction = "L"
+        elif position[y][x+1] == SecondLetter:
+            direction = "R"
+        elif position[y+1][x-1] == SecondLetter:
+            direction = "DL"
+        elif position[y+1][x] == SecondLetter:
+            direction = "D"
+        elif position[y+1][x+1] == SecondLetter:
+            direction = "DR"
     return direction
 
 def searchDirection(direction,word,position,y,x):
-    CheckAgen = True
-    while CheckAgen == True:
-        for char in range (word - 2):
-            letter = lookFor[word][char]
-            if direction ==  "upL":
-                print "got here"
-                if letter == position[y-char][x-char]:
-                    CheckAgen = True
-            elif direction == "up":
-                if letter == position[y-char][x]:
-                    CheckAgen = True
-            elif direction == "upR":
-                if letter == position[y-char][x+char]:
-                    CheckAgen = True
-            elif direction == "L":
-                if letter == position[y][x-char]:
-                    CheckAgen = True
-            elif direction == "R":
-                if letter == position[y][x+char]:
-                    CheckAgen = True
-            elif direction == "DL":
-                if letter == position[y+char][x-char]:
-                    CheckAgen = True
-            elif direction == "D":
-                if letter == position[y+char][x]:
-                    CheckAgen = True
-            elif direction == "DR":
-                if letter == position[y+char][x+char]:
-                    CheckAgen = True
-            else:
-                CheckAgen = False
-            
+    if direction != 0:
+        for char in range (len(lookFor[word]) - 1):
+                letter = lookFor[word][char]
+                if direction ==  "upL" and y-char != -1 and x+char != width+1:
+                    if letter == position[y-char][x-char]:
+                        print position[y-char][x-char]
+                    else:
+                        direction = 0
+                elif direction == "up":
+                    if letter == position[y-char][x]:
+                        print position[y-char][x]
+                    else:
+                        direction = 0
+                elif direction == "upR" and x+char != width:
+                    if letter == position[y-char][x+char]:
+                        print position[y-char][x+char]
+                    else:
+                        direction = 0
+                elif direction == "L":
+                    if letter == position[y][x-char]:
+                        print position[y][x-char]
+                    else:
+                        direction = 0
+                elif direction == "R" and x+char != width:
+                    if letter == position[y][x+char]:
+                        print position[y][x+char]
+                    else:
+                        direction = 0
+                elif direction == "DL":
+                    if letter == position[y+char][x-char]:
+                        print position[y+char][x-char]
+                    else:
+                        direction = 0
+                elif direction == "D":
+                    if letter == position[y+char][x]:
+                        print position[y+char][x]
+                    else:
+                        direction = 0
+                elif direction == "DR" and y+char != height:
+                    if letter == position[y+char][x+char]:
+                        print position[y+char][x+char]
+                    else:
+                        direction = 0
+                else:
+                    direction = 0
+
 for y in range (height):
     for x in range (width):
         for word in range (len(lookFor)):
@@ -129,30 +160,24 @@ for y in range (height):
                 FirstLetter = lookFor[word][0]
                 SecondLetter = lookFor[word][1]
                 if y == 0 and x == 0:
-                    print "top leaft" + str(FirstLetter)
                     side = "top leaft"
                 elif y == 0 and x == width-1:
-                    print "top right" + str(FirstLetter)
                     side = "top right"
                 elif y == height-1 and x == 0:
-                    print "botom leaft" + str(FirstLetter)
                     side = "botom leaft"
                 elif y == height-1 and x == width-1:
-                    print "botom right" + str(FirstLetter)
                     side = "botom leaft"
                 elif y == 0:
-                    print "top" + str(FirstLetter)
                     side ="top"
                 elif y == height-1:
-                    print "botom" + str(FirstLetter)
                     side = "botom"
                 elif x == 0:
-                    print "leaft side" + str(FirstLetter)
                     side = "leaft side"
                 elif x == width-1:
-                    print "right side" + str(FirstLetter)
                     side = "right side"
                 else:
-                    print "middle" + str(FirstLetter)
+                    side = "middle"
+                print side + " x=" + str(x+1) + " y=" + str(y*(-1)-1)
                 direction = search(side,position,y,x)
                 searchDirection(direction,word,position,y,x)
+                direction = 0
